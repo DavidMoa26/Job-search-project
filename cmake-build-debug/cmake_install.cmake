bfe4b251633e9943b80625072892beb34e00eeac
DanielHouri5 <hdanielh50@gmail.com>
2024-02-21 14:19:10 +0200

    k
    k

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/hdani/CLionProjects/Job_Search
+# Install script for directory: C:/Users/hdani/OneDrive/מסמכים/GitHub/Job-search-project
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -39,7 +39,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("C:/Users/hdani/CLionProjects/Job_Search/cmake-build-debug/SQLiteCpp/cmake_install.cmake")
+  include("C:/Users/hdani/OneDrive/מסמכים/GitHub/Job-search-project/cmake-build-debug/SQLiteCpp/cmake_install.cmake")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/hdani/CLionProjects/Job_Search/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/hdani/OneDrive/מסמכים/GitHub/Job-search-project/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
