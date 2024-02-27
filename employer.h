e16507972e78ce8960c5a081d41c5551ba06a23b
danielbogus99 <danik444@walla.co.il>
2024-02-27 14:40:37 +0200

    update

--- employer.h
+++ employer.h
@@ -13,5 +13,12 @@ void CreateJobsListTable(Database&);
 void PostJob(Database&, string&);
 void InsertJobToDatabase(Database& , string& , string& ,string&, string&, string&, string& , string&, string&);
 void FetchAllJobs(Database& , string&);
-
+bool InterviewInvitationsExist(Database&db);
+void CreateInterviewInvitationTable(Database& db);
+void printCandidateResume(Database&db ,string&selected_candidate_id);
+void SendInterviewInvitation(Database&db,string&id);
+void ViewAllInterviewInvitation(Database& db, string&employee_id);
+string FetchJobsEmployee(Database& db, string& id);
+bool printPendingCandidates(Database&db,string &job_idSelected);
+void insertToInterviewInvitationtable(Database&db,string&candidate_id,string&job_id,string&employee_id);
 #endif //xMAIN_CPP_EMPLOYER_H
